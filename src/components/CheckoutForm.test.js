78d075218f4934a7b0b94ab7046251c82aa636d8
Nelson Maldonado <play2nino@gmail.com>
2021-10-29 17:09:13 -0500

    test still working

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,21 @@
-import React from "react";
-import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
-import CheckoutForm from "./CheckoutForm";
+import React from "react"
+import MutationObserver from "mutationobserver-shim"
+import { render } from "@testing-library/react"
+import CheckoutForm from "./CheckoutForm"
+import PlantList from "./PlantList"
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
+  render(<CheckoutForm />)
+})
 
-test("shows success message on submit with form details", () => {});
+test("shows success message on submit with form details", () => {
+  render(<CheckoutForm />)
+})
+
+describe("Plant List tests", () => {
+  test("renders without errors", () => {
+    render(<PlantList />)
+  })
+})
